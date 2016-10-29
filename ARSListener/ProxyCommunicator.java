36264bffbed2bcf2f6c740554013f4f406d4d889
tom08 <kroll001@live.missouristate.edu>
2016-10-29 18:43:44 -0500

    Setup database connection for ARS listener and server queries data to send

--- ARSListener/ProxyCommunicator.java
+++ ARSListener/ProxyCommunicator.java
@@ -4,6 +4,14 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.NullPointerException;
 
 public class ProxyCommunicator{
     public static void main(String[] args) throws Exception {
@@ -29,41 +37,100 @@ public class ProxyCommunicator{
             log("New connection at " + socket);
         }
 
+        private List<String> add_pages(List<String> pages, java.sql.ResultSet results){
+            String entry = "";
+            try{
+            while(results.next()){
+                entry += results.getLong("id");
+                entry += ",," + results.getString("url");
+                pages.add(entry);
+                entry = "";
+            }
+            } catch (SQLException ex){
+                log(ex.getMessage());
+            }
+            return pages;
+        }
+
+        private List<String> add_ads(List<String> ads, java.sql.ResultSet results){
+            String entry = "";
+            try{
+            while(results.next()){
+                entry += results.getLong("id");
+                entry += ",," + results.getString("page_location");
+                entry += ",," + results.getDouble("focus_ratio");
+                entry += ",," + results.getDouble("active_ratio");
+                entry += ",," + results.getDouble("total_spent");
+                entry += ",," + results.getLong("page_id");
+                ads.add(entry);
+                entry = "";
+            }
+            } catch (SQLException ex){
+                log(ex.getMessage());
+            }
+            return ads;
+        }
+
+        private List<String> add_keywords(List<String> keywords, java.sql.ResultSet results){
+            String entry = "";
+            try{
+            while(results.next()){
+                entry += results.getLong("id");
+                entry += ",," + results.getString("keyword_name");
+                keywords.add(entry);
+                entry = "";
+            }
+            } catch (SQLException ex){
+                log(ex.getMessage());
+            }
+            return keywords;
+        }
+
         public void run(){
             try{
                 BufferedReader in = new BufferedReader(
                 new InputStreamReader(socket.getInputStream()));
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                 String cmd;
-                String[] pages = {
-                    "TYPE,,PAGE,,2",
-                    "1,,http://missouristate.edu",
-                    "2,,http://google.com",
-                };
-                String[] ad_locations = {
-                    "TYPE,,AD,,4",
-                    "1,,A,,0.334,,0.45,,15,,1",
-                    "2,,B,,0.223,,0.55,,12,,1",
-                    "3,,C,,0.221,,0.11,,11,,1",
-                    "4,,D,,0.123,,0.8888,,32,,1",
-                    }; 
-                String[] keywords = {
-                    "TYPE,,KEY,,2",
-                    "1,,test keyword 1",
-                    "2,,test kwd 2",
-                };
-                String[][] to_send = {
-                    pages,
-                    keywords,
-                    ad_locations,
-                };
+                MysqlConnection conn = new MysqlConnection();
+                java.sql.ResultSet page_results = null;
+                java.sql.ResultSet ad_results = null;
+                java.sql.ResultSet key_results = null;
+                try{
+                    page_results = conn.getAllPages();
+                    ad_results = conn.getAllAdLocationVisits();
+                    key_results = conn.getAllKeywords();
+                } catch(NullPointerException ex){
+                    log(ex.getMessage());
+                }
+                List<String> pages = new ArrayList<String>();
+                List<String> ad_locations = new ArrayList<String>();
+                List<String> keywords = new ArrayList<String>();
+                pages.add("TYPE,,PAGE");
+                ad_locations.add("TYPE,,AD");
+                keywords.add("TYPE,,KEY");
+                if(page_results != null){
+                    pages = add_pages(pages, page_results);
+                }
+                if(ad_results != null){
+                    ad_locations = add_ads(ad_locations, ad_results);
+                }
+                if(key_results != null){
+                    keywords = add_keywords(keywords, key_results);
+                }
+
+
+                List<List<String>> to_send = new ArrayList<List<String>>();
+                to_send.add(pages);
+                to_send.add(ad_locations);
+                to_send.add(keywords);
 
                 out.println("Please enter your command.\n");
                 cmd = in.readLine();
-                for(int i = 0; i < to_send.length; i++){
-                    log(cmd);
-                    for(int j = 0; j < to_send[i].length; j++){
-                        out.println(to_send[i][j]);
+                for(int i = 0; i < to_send.size(); i++){
+                    for(int j = 0; j < to_send.get(i).size(); j++){
+                        log(to_send.get(i).get(j));
+                        out.println(to_send.get(i).get(j));
                     }
                 }
             } catch (IOException e) {
@@ -80,6 +147,68 @@ public class ProxyCommunicator{
 
         private void log(String message) {
             System.out.println(message);
+        }
+    }
+
+    public static class MysqlConnection {
+        java.sql.Connection connection;
+
+        public MysqlConnection(){
+            try{
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+                this.connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/ProxyAdData?","root","password");
+            }
+            catch (Exception ex){
+                System.out.println("There was an error establishing the DB connection.");
+                System.out.println("SQLException: " + ex.getMessage());
+                ex.printStackTrace();
+            }
+        }
+
+        private java.sql.Connection conn(){
+            return this.connection;
+        }
+
+        public java.sql.ResultSet getAllPages(){
+            java.sql.Statement stmt = null;
+            java.sql.ResultSet results = null;
+            try {
+                    stmt = conn().createStatement();
+                    results = stmt.executeQuery("SELECT * FROM page");
+
+                }
+            catch (SQLException ex){
+                System.out.println("SQLException: " + ex.getMessage());
+            }
+            return results;
+        }
+
+        public java.sql.ResultSet getAllKeywords(){
+            java.sql.Statement stmt = null;
+            java.sql.ResultSet results = null;
+            try {
+                    stmt = conn().createStatement();
+                    results = stmt.executeQuery("SELECT * FROM keyword");
+
+                }
+            catch (SQLException ex){
+                System.out.println("SQLException: " + ex.getMessage());
+            }
+            return results;
+        }
+
+        public java.sql.ResultSet getAllAdLocationVisits(){
+            java.sql.Statement stmt = null;
+            java.sql.ResultSet results = null;
+            try {
+                    stmt = conn().createStatement();
+                    results = stmt.executeQuery("SELECT * FROM ad_location_visit");
+
+                }
+            catch (SQLException ex){
+                System.out.println("SQLException: " + ex.getMessage());
+            }
+            return results;
         }
     }
 }
